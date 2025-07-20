016d860aedc70357e7ae99ec1dfddc8766ce7f6f
Agustin Collueque <agustincv1997@gmail.com>
2025-07-20 14:08:04 -0300

    Se actualizaron estilos del contact-componente.

--- src/app/features/contact/components/form-contact/form-contact.component.ts
+++ src/app/features/contact/components/form-contact/form-contact.component.ts
@@ -1,20 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form-contact',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-contact.component.html',
-  styleUrls: ['./form-contact.component.less']
+  styleUrls: ['./form-contact.component.less'],
 })
-export class FormContactComponent implements OnInit {
-
-  @Output() submit = new EventEmitter<{name: string; email: string; message: string}>();
+export class FormContactComponent implements OnInit, OnDestroy {
+  @Output() contactSubmit = new EventEmitter<{
+    name: string;
+    email: string;
+    message: string;
+  }>();
   contactForm!: FormGroup;
 
-  constructor(private fb :FormBuilder){}
+  constructor(private fb: FormBuilder) {}
+  ngOnDestroy(): void {
+    this.contactForm.reset();
+  }
 
   ngOnInit(): void {
     this.contactForm = this.fb.group({
@@ -25,8 +42,9 @@ export class FormContactComponent implements OnInit {
   }
 
   enviar() {
-    if(this.contactForm.valid) {
-      this.submit.emit(this.contactForm.value);
+    if (this.contactForm.valid) {
+      this.contactSubmit.emit(this.contactForm.value);
+      this.contactForm.reset();
     } else {
       this.contactForm.markAllAsTouched();
     }
