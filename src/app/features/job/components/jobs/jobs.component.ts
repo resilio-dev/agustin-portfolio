b2d015553be10d680ff85b7d6d93ec4001c3858e
Agustin Collueque <agustincv1997@gmail.com>
2025-07-22 14:23:47 -0300

    Se realizó la vista final del componente jobs. Se realizó la traducción al inglés y se agregaron estilos definitivos.

--- src/app/features/job/components/jobs/jobs.component.ts
+++ src/app/features/job/components/jobs/jobs.component.ts
@@ -5,14 +5,19 @@ import { IJob } from 'src/app/core/models/IJob.model';
 import { TrabajoService } from 'src/app/core/services/job-service/trabajo.service';
 import { HttpErrorResponse } from '@angular/common/module.d-CnjH8Dlt';
 import { JobFormComponent } from '../job-form/job-form.component';
+import { CardComponent } from 'src/app/shared/components/card/card.component';
+import { ISkill } from 'src/app/core/models/ISkill.model';
+import { ModalActionsButtonComponent } from 'src/app/shared/components/modal-actions-button/modal-actions-button.component';
 
 @Component({
   selector: 'app-jobs',
   imports: [
     CommonModule,
     ModalComponent,
-    JobFormComponent
-  ],
+    JobFormComponent,
+    ModalActionsButtonComponent,
+    CardComponent
+],
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.less',
 })
@@ -29,6 +34,14 @@ export class JobsComponent implements OnInit {
       },
       error: (er: HttpErrorResponse) => console.error(er.message),
     });
+  }
+
+  getTecnologies(tecno: ISkill[]): string[] {
+    const tecnoArray = [];
+    for (let tec of tecno) {
+      tecnoArray.push(tec.name)
+    }
+    return tecnoArray;
   }
 
   agregarTrabajo(job: IJob) {
