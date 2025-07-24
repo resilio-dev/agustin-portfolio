d7702303004cfa3c70cd6b675ff7cbdec1c96685
Agustin Collueque <agustincv1997@gmail.com>
2025-07-24 02:17:31 -0300

    Se instalo ngx-toastr para mostrar cards cuando se interactúe con la api.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,18 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/routes';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-center-screen',
+      timeOut: 4000,
+      preventDuplicates: true,
+    })
   ]
 }).catch(err => console.error(err));
