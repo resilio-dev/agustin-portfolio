6baa67c3d3b703f65eb7f3eb40a08c4abf1b9a46
Agustin Collueque <agustincv1997@gmail.com>
2025-07-31 22:33:02 -0300

    Se actualizó el contenido de assets

--- src/app/core/constants/default-user.data.ts
+++ src/app/core/constants/default-user.data.ts
@@ -13,7 +13,7 @@ export const DEFAULT_USER_DATA: IUser = {
     'I enjoy creating web applications that not only work well, but are carefully designed from the inside out. I have a special love for backend development with Java, where I focus on writing clean, maintainable code, good architecture, and proven design patterns. I care about the details, from how data flows to how users feel, combining depth and technique with a human touch.',
   email: 'agustincv1997@gmail.com',
   age: 27,
-  urlImg: 'assets/img/perfil-default.jpeg',
+  urlImg: 'assets/img/prophile-default.webp',
   urlCV: 'assets/CV_AGUSTIN.pdf',
   projects: [
     {
