d35bc82e2b1f6df32f386f4ac0c675d56474ee12
Agustin Collueque <agustincv1997@gmail.com>
2025-08-04 23:56:13 -0300

    Se realizo la vista responsive de la barra de navegación superior.

--- src/app/layouts/desktop/desktop-layout.component.ts
+++ src/app/layouts/desktop/desktop-layout.component.ts
@@ -1,15 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarBottomComponent } from 'src/app/features/navbar-bottom/navbar-bottom.component';
 import { NavbarComponent } from 'src/app/features/navbar/navbar.component';
 
 @Component({
   selector: 'app-desktop-layout',
   standalone: true,
-  imports: [NavbarComponent, NavbarBottomComponent, RouterOutlet],
+  imports: [NavbarComponent, NavbarBottomComponent, RouterOutlet, CommonModule],
   templateUrl: './desktop-layout.component.html',
   styleUrls: ['./desktop-layout.component.less'],
 })
 export class DesktopLayoutComponent {
+  constructor(private router: Router) {}
+
+esHome() {
+  return this.router.url === '/desktop' || ''
+}
 
 }
